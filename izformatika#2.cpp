7449445bd6507459259909ec2cf1e5d59cc46339
BorisenkoSergey <61838037+BorisenkoSergey@users.noreply.github.com>
2020-03-05 18:03:25 +0300

    Update izformatika#2.cpp
    я заполняю вектор

--- izformatika#2.cpp
+++ izformatika#2.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
+  vector<int> vct;
+  while(true)
+  {
+    int val;
+    cin >> val;
+    if(val == -9999)
+      break;
+    vct.push_back(val);
+  }
+  
 return 0;
 }
