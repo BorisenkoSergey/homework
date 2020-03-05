b9de6b59c7004b4560a14768236f61b23deda45c
BorisenkoSergey <61838037+BorisenkoSergey@users.noreply.github.com>
2020-03-05 18:44:16 +0300

    Update izformatika#1.cpp
    бегаем по вектору и отбираем лучшую пару + добавил несколько комментов

--- izformatika#1.cpp
+++ izformatika#1.cpp
@@ -14,5 +14,16 @@ int main()
     vct.push_back(val);
   }
   
+  int  check_sum = 999999; //просто большое число
+  for(int l = 0 ; l < vct.size(); ++l)
+  {
+    for(int m = l+1 ; m < vct.size(); ++m)
+    {
+      int this_pair = vct[l] * vct[m];
+      if( (this_pair < check_sum) && (this_pair % 6) ) //главное условие
+        check_sum = this_pair;        
+    }
+  } 
+  cout << "result " << check_sum;
   return 0;
 }
